fc7f87d5f5148b5fb3937896d4b387fb62a7af13
Dmitrii <ddapd@yandex.ru>
2018-04-01 20:52:14 +0300

    5й урок

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -4,30 +4,25 @@ let hype = document.getElementsByClassName('adv'),
 	title = document.getElementsByClassName('title'),
 	column = document.getElementsByClassName('column')[1];
 
-// let text = document.createTextNode('Мы продаем только подлинную технику Apple');
-// text.classList.add("title");
-column.removeChild(title[0]);				// del Apple
+document.body.style.backgroundImage = 'url(../lesson5/img/apple_true.jpg)'; // jpg
 
-// let div = document.createElement('div'); 	// new Apple
-// div.innerHTML = 'Мы продаем только подлинную технику Apple';				// Apple
-// div.classList.add("title");					// Apple
-// column.insertBefore(li, ulLi[4]);	
-
+let div = document.createElement('div'); 	// new Apple
+div.innerHTML = 'Мы продаем только подлинную технику Apple';	// Apple
+div.classList.add("title");					// Apple
+column.replaceChild(div, title[0]);			// Apple
 
 let li = document.createElement('li'); 	// пятый пункт
 li.innerHTML = 'Пятый пункт';			// пятый пункт
 li.classList.add("menu-item");			// пятый пункт
 menu.insertBefore(li, ulLi[4]);			// пятый пункт
 
+menu.insertBefore(ulLi[2], ulLi[1]);	//пункт меню 2-3
+column.removeChild(hype[0]); 			//реклама
 
-
-menu.insertBefore(ulLi[2], ulLi[1]); //пункт меню 2-3
-column.removeChild(hype[0]); //реклама
-
-
-
-console.log(hype[0]);
-console.log(ulLi[2]);
-console.log(li);
-console.log(text);
-console.log(div);
+//
+// console.log(hype[0]);
+// console.log(ulLi[2]);
+// console.log(li);
+// console.log(text);
+// console.log(title[0]);
+// console.log(div);
