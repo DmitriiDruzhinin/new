2c67e319ad86bd51bdf94df0dfd643a4b113bf39
Dmitrii <ddapd@yandex.ru>
2018-04-01 22:40:57 +0300

    5й урок

---
+++ lesson5_2/js/script.js
@@ -0,0 +1,23 @@
+let openBtn = document.getElementById('open-btn'),
+	name = document.getElementsByClassName('name-value')[0],
+	budget = document.getElementsByClassName('budget-value')[0],
+	goods = document.getElementsByClassName('goods-value')[0],
+	items = document.getElementsByClassName('items-value')[0],
+	employers = document.getElementsByClassName('employers-value')[0],
+	discount = document.getElementsByClassName('discount-value')[0],
+	isopen = document.getElementsByClassName('isopen-value')[0],
+	goodsItem = document.getElementsByClassName('goods-item'),
+	button = document.getElementsByTagName('button'),
+	selectorPro = document.querySelector('#items'),
+	selectorTime = document.querySelector('#time'),
+	selectorDay = document.querySelector('#budget'),
+	selectorAll = document.querySelectorAll('div > input');
+
+
+
+console.log(openBtn, name, budget, goods, items, employers, discount, isopen);
+console.log(goodsItem[0],goodsItem[1],goodsItem[2],goodsItem[3]);
+console.log(button[1],button[2],button[3]);
+console.log(selectorPro,selectorTime,selectorDay);
+console.log(selectorAll[7],selectorAll[8],selectorAll[9]);
+
