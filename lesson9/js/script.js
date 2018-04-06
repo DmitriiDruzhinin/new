3effbd51cdd87a748388b2947f3ff537572dd5d1
Dmitrii <ddapd@yandex.ru>
2018-04-06 21:25:02 +0300

    9й исправленный урок

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -82,22 +82,44 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	// Modal
 
-	let more = document.querySelector('.more'),
+	let des_btn = document.getElementsByClassName('description-btn'),
+		more = document.querySelector('.more'),
 		overlay = document.querySelector('.overlay'),
 		close = document.querySelector('.popup-close');
+	
 
-	more.addEventListener('click', function() {
-		this.classList.add('more-splash');
-		overlay.style.display = "block";
-		document.body.style.overflow = 'hidden';
+		for (let i = 0; i < des_btn.length; i++) { 
+			des_btn[i].addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = 'block';
+				document.body.style.overflow = 'hidden';
 
-	});
+			});
 
-	close.addEventListener('click', function() {
-		overlay.style.display = "none";
-		more.classList.remove('more-splash');
-		document.body.style.overflow = '';
+				close.addEventListener('click', function() {
+				overlay.style.display = "none";
+				des_btn[i].classList.remove('more-splash');
+				document.body.style.overflow = '';
 
-	});
+			});
+
+	   	}	
+	    
+//
+		more.addEventListener('click', function() {
+				this.classList.add('more-splash');
+				overlay.style.display = "block";
+				document.body.style.overflow = 'hidden';
+
+			});
+
+			close.addEventListener('click', function() {
+				overlay.style.display = "none";
+				more.classList.remove('more-splash');
+				document.body.style.overflow = '';
+
+			});
+					
+
 
 });
