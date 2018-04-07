fcb35ae0318f72ea099e2f26d008acfc65b37a9c
Dmitrii <ddapd@yandex.ru>
2018-04-07 23:07:46 +0300

    10й исправленный урок

--- lesson10/script.js
+++ lesson10/script.js
@@ -1,25 +1,26 @@
 class Options{
+
 	constructor(height, width, bg, fontSize, textAlign) {
 		this.height = height;
 		this.width = width;
 		this.bg = bg;
 		this.fontSize = fontSize;
 		this.textAlign = textAlign;
-		// console.log(height, width, bg, fontSize, textAlign);
+		
 	}
 
 	newDiv(text){
 		let div = document.createElement('div'); 	
-		
+		div.style.display = 'inline-block';
 		div.style.height = this.height;
 		div.style.width = this.width;
 		div.style.backgroundColor = this.bg;
 		div.style.fontSize = this.fontSize;
 		div.style.textAlign = this.textAlign;
 		div.textContent= text;
+		
 		document.body.appendChild(div);
 
-		
 		console.log(div);
 	}
 
@@ -29,6 +30,6 @@ const creationDiv = new Options('200px', '300px', 'red', '50pt', 'center');
 
 console.log(creationDiv.newDiv('Новый Div'));
 
-// div.style.display = 'inline-block';
+
         
         
