6aaefbde11b97947cee8f2900d510aeb35503a43
Dmitrii <ddapd@yandex.ru>
2018-04-12 23:08:18 +0300

    12й урок

--- lesson12/js/script.js
+++ lesson12/js/script.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
 
-	$('.main_btna:eq(0)').on('click', function(){
+	$('.text-center').on('click', function(){
 		$('.modal').slideDown( 500, "linear"),
 		$('.overlay').fadeIn("slow");
 	});
 
-	$('.main_btn:eq(0)').on('click', function(){
-		$('.modal').slideDown( 500, "linear"),
-		$('.overlay').fadeIn("slow");
-	});
-	
 	$('nav li:eq(1)').on('click', function(){
 		$('.modal').slideDown( 500, "linear"),
 		$('.overlay').fadeIn("slow");
