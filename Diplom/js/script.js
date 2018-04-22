d3f3137099bac6a8809d1f1526b2875925484dce
Dmitrii <ddapd@yandex.ru>
2018-04-22 12:49:44 +0300

    Diplom

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', function() {
 			if (isNaN(name) && name != '' && name.length < 50) {
 				candidate.nameCandidate = name;
 			} else {
-				// alert("Заполните правильно ФИО"); 
+				// alert("Заполните ФИО"); 
 			};
 		});
 
@@ -54,7 +54,7 @@ window.addEventListener('DOMContentLoaded', function() {
 			if (!isNaN(age) && age != '') {
 				candidate.ageCandidate = age;
 			} else {
-				// alert("Заполните правильно возраст");
+				// alert("Заполните возраст");
 			};
 		});
 
@@ -69,7 +69,7 @@ window.addEventListener('DOMContentLoaded', function() {
 			bio = bioId.value;
 			
 			if (bio.length < 100 && bio != '') {
-				candidate.biosCandidate = bio.split(" ");
+				candidate.biosCandidate = bio;
 			} else {
 				// alert("Заполните правильно биографию");
 			};
@@ -78,21 +78,21 @@ window.addEventListener('DOMContentLoaded', function() {
 	// gender man/woman
 	
 		let radioOption = [document.getElementsByName('sex')[0],document.getElementsByName('sex')[1]];
-		var a = 5,
+		var a = 7,
 			b = 8;	
   
 		radioOption.forEach(function(e) {
 			e.addEventListener("click", function() {
 			candidate.genderCandidate = e.value;
 				if(candidate.genderCandidate == "Мужской"){
-					a = 5,
+					a = 7,
 					b = 8;
 					preview.style.backgroundImage = "url('../Diplom/img/construct-5.png')";
 					personEasy.style.backgroundImage = preview.style.backgroundImage;
 					candidate.urlImg  = preview.style.backgroundImage;
 				} else {
 					a = 1,
-					b = 4;
+					b = 6;
 					preview.style.backgroundImage = "url('../Diplom/img/construct-4.png')";
 					personEasy.style.backgroundImage = preview.style.backgroundImage;
 					candidate.urlImg  = preview.style.backgroundImage;
@@ -142,85 +142,94 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		ready.addEventListener('click', function() {
 		
-		// 	activation card	
-				overlay.style.display = "none";
-				main.style.display = "block";
-				custom.style.display = 'none';
-				customInfo.style.display = 'none';
-				customChar.style.display = 'none';
-				customStyle.style.display = 'none';
+			if(candidate.nameCandidate == '') {
+				alert("Заполните ФИО");
+			// } else if (candidate.ageCandidate == '' ) {
+			// 	alert("Заполните возраст");
+			// } else if (candidate.biosCandidate == '') {
+			// 	alert("Заполните Биографию");
+			} else {
 
-		let mainCardsItem = document.getElementsByClassName('main-cards-item'),		// new card
-			mainCards = document.getElementsByClassName('main-cards')[0],
-			div = document.createElement('div');
-			div.classList.add("main-cards-item");					
-			mainCards.insertBefore(div, mainCardsItem[1]);
-			
-		let divBlock = document.createElement('div');								// new div class="candidate-block"
-			divBlock.classList.add("candidate-block");
-			mainCardsItem[1].appendChild(divBlock);
+				// 	activation card	
+						overlay.style.display = "none";
+						main.style.display = "block";
+						custom.style.display = 'none';
+						customInfo.style.display = 'none';
+						customChar.style.display = 'none';
+						customStyle.style.display = 'none';
 
-		let divPhoto = document.createElement('div');								// new div class="photo photo-1"
-			divPhoto.classList.add("photo");
-			divPhoto.style.backgroundSize = "cover";
-			divPhoto.style.backgroundImage = candidate.urlImg;
-			// divPhoto.style.backgroundPosition = "center";
-			// divPhoto.style.backgroundRepeat = "no-repeat";
-			divBlock.appendChild(divPhoto);
+				let mainCardsItem = document.getElementsByClassName('main-cards-item'),		// new card
+					mainCards = document.getElementsByClassName('main-cards')[0],
+					div = document.createElement('div');
+					div.classList.add("main-cards-item");					
+					mainCards.insertBefore(div, mainCardsItem[1]);
+					
+				let divBlock = document.createElement('div');								// new div class="candidate-block"
+					divBlock.classList.add("candidate-block");
+					mainCardsItem[1].appendChild(divBlock);
 
-		let divResult = document.createElement('div');								// new div class="result"
-			divResult.classList.add("result");
-			divBlock.appendChild(divResult);
+				let divPhoto = document.createElement('div');								// new div class="photo photo-1"
+					divPhoto.classList.add("photo");
+					divPhoto.style.backgroundSize = "contain";
+					divPhoto.style.backgroundImage = candidate.urlImg;
+					// divPhoto.style.backgroundPosition = "center";
+					divPhoto.style.backgroundRepeat = "no-repeat";
+					divBlock.appendChild(divPhoto);
 
-		let divResultCount = document.createElement('div');							// new div class="result-count"
-			divResultCount.innerHTML = '0 %';
-			divResultCount.classList.add("result-count");
-			divResult.appendChild(divResultCount);
+				let divResult = document.createElement('div');								// new div class="result"
+					divResult.classList.add("result");
+					divBlock.appendChild(divResult);
 
-		let divResultProgress = document.createElement('div');						// new div class="progress"
-			divResultProgress.classList.add("progress");
-			divResult.appendChild(divResultProgress);
-		
-		let divResultProgressBar = document.createElement('div');					// new div class="progress-bar"
-			divResultProgressBar.classList.add("progress-bar");
-			divResultProgress.appendChild(divResultProgressBar);
+				let divResultCount = document.createElement('div');							// new div class="result-count"
+					divResultCount.innerHTML = '0 %';
+					divResultCount.classList.add("result-count");
+					divResult.appendChild(divResultCount);
 
-		let divName = document.createElement('div');								// new div class="name"
-			divName.textContent =  candidate.nameCandidate;
-			divName.classList.add("name");
-			mainCardsItem[1].appendChild(divName);
+				let divResultProgress = document.createElement('div');						// new div class="progress"
+					divResultProgress.classList.add("progress");
+					divResult.appendChild(divResultProgress);
+				
+				let divResultProgressBar = document.createElement('div');					// new div class="progress-bar"
+					divResultProgressBar.classList.add("progress-bar");
+					divResultProgress.appendChild(divResultProgressBar);
 
-		let divAge = document.createElement('div');									// new div class="age"
-			divAge.textContent =  candidate.ageCandidate + " лет";
-			divAge.classList.add("age");
-			mainCardsItem[1].appendChild(divAge);
+				let divName = document.createElement('div');								// new div class="name"
+					divName.textContent =  candidate.nameCandidate;
+					divName.classList.add("name");
+					mainCardsItem[1].appendChild(divName);
 
-		let divSexName = document.createElement('div');								// new div name_sex
-			divSexName.textContent = "Пол:";
-			mainCardsItem[1].appendChild(divSexName);
-		
-		let divSex = document.createElement('div');									// new div class="sex"
-			divSex.textContent =  candidate.genderCandidate;
-			divSex.classList.add("sex");
-			mainCardsItem[1].appendChild(divSex);
-			
-		let divViewsName = document.createElement('div');							// new div name_views
-			divViewsName.textContent = "Полит. взгляды:";
-			mainCardsItem[1].appendChild(divViewsName);
-		
-		let divViews = document.createElement('div');								// new div class="views"
-			divViews.textContent =  candidate.positionCandidate;
-			divViews.classList.add("views");
-			mainCardsItem[1].appendChild(divViews);
+				let divAge = document.createElement('div');									// new div class="age"
+					divAge.textContent =  candidate.ageCandidate + " лет";
+					divAge.classList.add("age");
+					mainCardsItem[1].appendChild(divAge);
 
-		let divBioName = document.createElement('div');							// new div name_bio
-			divBioName.textContent = "Биография";
-			mainCardsItem[1].appendChild(divBioName);
-		
-		let divBio = document.createElement('div');									// new div class="bio"
-			divBio.textContent =  candidate.biosCandidate;
-			divBio.classList.add("bio");
-			mainCardsItem[1].appendChild(divBio);
+				let divSexName = document.createElement('div');								// new div name_sex
+					divSexName.textContent = "Пол:";
+					mainCardsItem[1].appendChild(divSexName);
+				
+				let divSex = document.createElement('div');									// new div class="sex"
+					divSex.textContent =  candidate.genderCandidate;
+					divSex.classList.add("sex");
+					mainCardsItem[1].appendChild(divSex);
+					
+				let divViewsName = document.createElement('div');							// new div name_views
+					divViewsName.textContent = "Полит. взгляды:";
+					mainCardsItem[1].appendChild(divViewsName);
+				
+				let divViews = document.createElement('div');								// new div class="views"
+					divViews.textContent =  candidate.positionCandidate;
+					divViews.classList.add("views");
+					mainCardsItem[1].appendChild(divViews);
+
+				let divBioName = document.createElement('div');								// new div name_bio
+					divBioName.textContent = "Биография";
+					mainCardsItem[1].appendChild(divBioName);
+				
+				let divBio = document.createElement('div');									// new div class="bio"
+					divBio.textContent =  candidate.biosCandidate;
+					divBio.classList.add("bio");
+					mainCardsItem[1].appendChild(divBio);
+			};
 		});
 		
 });
@@ -230,9 +239,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	var candidate = {
 		nameCandidate: '',
-		ageCandidate: age,
+		ageCandidate: '',
 		genderCandidate: "Мужской",
 		positionCandidate: 'Либеральные',
-		biosCandidate: [],
-		urlImg: ""
+		biosCandidate: '',
+		urlImg: ''
 	};
